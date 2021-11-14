7fb9bb7abe42e0ff702e032a37167560864d8b9f
PetroBabiuk <petro.babiuk2506@gmail.com>
2021-11-14 15:51:46 +0200

    change phone validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,7 +6,7 @@ const contactsOperations = require('../../model')
 const joiSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.number().positive().pattern(/^[0-9]+$/).required(),
+  phone: Joi.number().positive().required(),
 })
 
 const router = express.Router()
