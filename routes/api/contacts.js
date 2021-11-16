09663e58fb305671486360966c6a0479819ed0da
PetroBabiuk <petro.babiuk2506@gmail.com>
2021-11-16 12:43:43 +0200

    fix/ fix phone validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,7 +6,7 @@ const contactsOperations = require('../../model')
 const joiSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.number().positive().length(15).required(),
+  phone: Joi.string().length(15).pattern(/^\d+$/).required(),
 })
 
 const router = express.Router()
