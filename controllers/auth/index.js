254e15ca15eeddd51f17c1b6b47358565718fb20
Petro Babiuk <petro.babiuk2506@gmail.com>
2021-12-02 20:23:08 +0300

    add reexports

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,11 +3,15 @@ const login = require('./login')
 const logout = require('./logout')
 const current = require('./current')
 const updateAvatar = require('./updateAvatar')
+const verify = require('./verify')
+const repeatVerify = require('./repeatVerify')
 
 module.exports = {
   signup,
   login,
   logout,
   current,
-  updateAvatar
+  updateAvatar,
+  verify,
+  repeatVerify
 }
