d0030e0ab41478e9ce6394c19307c9c026e52d9b
Petro Babiuk <petro.babiuk2506@gmail.com>
2021-12-02 20:20:33 +0300

    add logic for send mail at signup

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -3,8 +3,10 @@ const bcrypt = require('bcryptjs')
 const fs = require('fs/promises')
 const path = require('path')
 const gravatar = require('gravatar')
+const { nanoid } = require('nanoid')
 
 const { User } = require('../../model')
+const { sendMail } = require('../../helpers')
 
 const avatarsDir = path.join(__dirname, '../../public/avatars')
 
@@ -14,14 +16,21 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict(`User with email=${email} already exist`)
   }
-
+  const verificationToken = nanoid()
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
   const avatarURL = gravatar.url(email, { protocol: 'https' })
   const newUser = await User.create({
     email,
     password: hashPassword,
-    avatarURL
+    avatarURL,
+    verificationToken
   })
+  const mail = {
+    to: email,
+    subject: 'Confirm signup',
+    text: `<a href="http://localhost3000/api/auth/verify/${verificationToken}">Click to confirm you email address</a>`
+  }
+  await sendMail(mail)
   const userFolder = path.join(avatarsDir, String(newUser._id))
   await fs.mkdir(userFolder)
   res.status(201).json({
