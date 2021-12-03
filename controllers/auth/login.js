88076cb51007a31e7e6855f02e46f48cb2749702
Petro Babiuk <petro.babiuk2506@gmail.com>
2021-12-03 07:48:59 +0300

    fix error message with not verify user

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,4 +1,4 @@
-const { Unauthorized } = require('http-errors')
+const { Unauthorized, BadRequest } = require('http-errors')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
@@ -11,8 +11,11 @@ const { SECRET_KEY } = process.env
 const login = async (req, res) => {
   const { email, password } = req.body
   const user = await User.findOne({ email })
-  if (!user || !user.verify) {
+  if (!user) {
     throw new Unauthorized('Email or password is wrong')
+  }
+  if (!user.verify) {
+    throw new BadRequest('Sorry, but your email was not verified')
   }
 
   const compareResult = bcrypt.compareSync(password, user.password)
